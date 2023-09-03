2c009325f4e2fb7eda5e0a5e9b08f742dd817a55
adz011 <99127403+adz011@users.noreply.github.com>
2023-09-03 17:22:18 +0200

    Enhanced Auction class for automatic table creation

--- src/main/java/com/bookstore/auction/Auction.java
+++ src/main/java/com/bookstore/auction/Auction.java
@@ -1,15 +1,33 @@
 package com.bookstore.auction;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Table
 @ToString
 public class Auction {
+
+    @Id
+    @SequenceGenerator(
+            name= "auction_sequence",
+            sequenceName = "auction_sequence",
+            allocationSize = 1
+    )
+
+
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "auction_sequence"
+    )
+
     @Getter
-    private String id;
+    private Long id;
 
     @Getter
     private String ownerEmail;
@@ -26,7 +44,9 @@ public class Auction {
     @Getter
     private BigDecimal price;
 
-    public Auction(String id, String ownerEmail, Long itemId, LocalDate startDate, LocalDate endDate, BigDecimal price) {
+
+
+    public Auction(Long id, String ownerEmail, Long itemId, LocalDate startDate, LocalDate endDate, BigDecimal price) {
         this.id = id;
         this.ownerEmail = ownerEmail;
         this.itemId = itemId;
@@ -35,4 +55,7 @@ public class Auction {
         this.price = price;
     }
 
+    public Auction() {
+
+    }
 }
