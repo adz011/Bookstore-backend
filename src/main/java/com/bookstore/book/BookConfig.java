edc6bd93caedca6ab1933dd96636a45b1d84e7fa
adz011 <99127403+adz011@users.noreply.github.com>
2023-09-03 17:21:05 +0200

    Book config added

---
+++ src/main/java/com/bookstore/book/BookConfig.java
@@ -0,0 +1,16 @@
+package com.bookstore.book;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BookConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(BookRepository bookRepository){
+        return args->{
+
+        };
+    }
+}
