bc20fd80d14ca2dff13c6b43dac0996b9535307d
adz011 <99127403+adz011@users.noreply.github.com>
2023-09-03 17:19:51 +0200

    Implemented controller for Book class

--- src/main/java/com/bookstore/book/BookController.java
+++ src/main/java/com/bookstore/book/BookController.java
@@ -1,5 +1,29 @@
 package com.bookstore.book;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
-    // TODO create controller class
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getBooks(){
+        return bookService.getBooks();
+    }
+
+    @PostMapping
+    public void registerNewBook(@RequestBody Book book){
+        bookService.addNewBook(book);
+    }
 }
