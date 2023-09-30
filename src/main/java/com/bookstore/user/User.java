74bc18d28eed5924c37fc24b996d0b8673107ba8
Mateusz113 <mateusz.ch.113@gmail.com>
2023-09-30 13:36:53 +0200

    Updated User facilities
    -Modified the User class to fit the security and design requirements
    -Added the Role class
    -Added the UserRepository interface

--- src/main/java/com/bookstore/user/User.java
+++ src/main/java/com/bookstore/user/User.java
@@ -1,17 +1,78 @@
 package com.bookstore.user;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table
-@Getter
-public class User {
+@Table(name = "user")
+public class User implements UserDetails {
     @Id
-    private long id;
-    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String nickname;
     private String email;
-    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(
+            String firstname,
+            String lastname,
+            String nickname,
+            String email,
+            String password,
+            Role role
+    ) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        //Returns email as it is unique identifier of a user
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
